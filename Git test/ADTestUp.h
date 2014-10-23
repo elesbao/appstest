ddc98f692a298edb91163c3a593150fe5a7ffe6b
Zarif Saraiva <zarifsaraiva@Mac-AD.local>
2014-10-22 22:19:13 -0200

    added new file

---
+++ Git test/ADTestUp.h
@@ -0,0 +1,13 @@
+//
+//  ADTestUp.h
+//  Git test
+//
+//  Created by Zarif Saraiva on 22/10/14.
+//  Copyright (c) 2014 AD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ADTestUp : NSObject
+
+@end
---
+++ Git test/ADTestUp.m
@@ -0,0 +1,13 @@
+//
+//  ADTestUp.m
+//  Git test
+//
+//  Created by Zarif Saraiva on 22/10/14.
+//  Copyright (c) 2014 AD. All rights reserved.
+//
+
+#import "ADTestUp.h"
+
+@implementation ADTestUp
+
+@end
